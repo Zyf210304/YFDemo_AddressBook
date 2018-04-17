2347906aae123f50fee645068503001fac48d351
Zyf210304 <672233580@qq.com>
2018-04-17 10:41:43 +0800

    亚飞
    通讯录

--- YFDemo_AddressBook/ViewController.h
+++ YFDemo_AddressBook/ViewController.h
@@ -7,8 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+/// iOS 9前的框架
+#import <AddressBook/AddressBook.h>
+#import <AddressBookUI/AddressBookUI.h>
+/// iOS 9的新框架
+#import <ContactsUI/ContactsUI.h>
 
-@interface ViewController : UIViewController
+#define Is_up_Ios_9             ([[UIDevice currentDevice].systemVersion floatValue] >= 9.0)
+
+@interface ViewController : UIViewController<ABPeoplePickerNavigationControllerDelegate,CNContactPickerDelegate>  
 
 
 @end
--- YFDemo_AddressBook/ViewController.m
+++ YFDemo_AddressBook/ViewController.m
@@ -10,6 +10,7 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) NSMutableArray *myDict;
 @end
 
 @implementation ViewController
@@ -17,13 +18,164 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self JudgeAddressBookPower];
 }
 
 
+#pragma mark ---- 调用系统通讯录
+- (void)JudgeAddressBookPower {
+    ///获取通讯录权限，调用系统通讯录
+    [self CheckAddressBookAuthorization:^(bool isAuthorized , bool isUp_ios_9) {
+        if (isAuthorized) {
+            [self callAddressBook:isUp_ios_9];
+        }else {
+            NSLog(@"请到设置>隐私>通讯录打开本应用的权限设置");
+        }
+    }];
+}
+
+- (void)CheckAddressBookAuthorization:(void (^)(bool isAuthorized , bool isUp_ios_9))block {
+    if (Is_up_Ios_9) {
+        CNContactStore * contactStore = [[CNContactStore alloc]init];
+        if ([CNContactStore authorizationStatusForEntityType:CNEntityTypeContacts] == CNAuthorizationStatusNotDetermined) {
+            [contactStore requestAccessForEntityType:CNEntityTypeContacts completionHandler:^(BOOL granted, NSError * __nullable error) {
+                if (error)
+                {
+                    NSLog(@"Error: %@", error);
+                }
+                else if (!granted)
+                {
+
+                    block(NO,YES);
+                }
+                else
+                {
+                    block(YES,YES);
+                }
+            }];
+        }
+        else if ([CNContactStore authorizationStatusForEntityType:CNEntityTypeContacts] == CNAuthorizationStatusAuthorized){
+            block(YES,YES);
+        }
+        else {
+            NSLog(@"请到设置>隐私>通讯录打开本应用的权限设置");
+        }
+    }else {
+        ABAddressBookRef addressBook = ABAddressBookCreateWithOptions(NULL, NULL);
+        ABAuthorizationStatus authStatus = ABAddressBookGetAuthorizationStatus();
+
+        if (authStatus == kABAuthorizationStatusNotDetermined)
+        {
+            ABAddressBookRequestAccessWithCompletion(addressBook, ^(bool granted, CFErrorRef error) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    if (error)
+                    {
+                        NSLog(@"Error: %@", (__bridge NSError *)error);
+                    }
+                    else if (!granted)
+                    {
+
+                        block(NO,NO);
+                    }
+                    else
+                    {
+                        block(YES,NO);
+                    }
+                });
+            });
+        }else if (authStatus == kABAuthorizationStatusAuthorized)
+        {
+            block(YES,NO);
+        }else {
+            NSLog(@"请到设置>隐私>通讯录打开本应用的权限设置");
+        }
+    }
+}
+
+- (void)callAddressBook:(BOOL)isUp_ios_9 {
+    if (isUp_ios_9) {
+//        CNContactPickerViewController *contactPicker = [[CNContactPickerViewController alloc] init];
+//        contactPicker.delegate = self;
+//        contactPicker.displayedPropertyKeys = @[CNContactPhoneNumbersKey];
+//        [self presentViewController:contactPicker animated:YES completion:nil];
+        [self getAddressBook];
+    }else {
+        ABPeoplePickerNavigationController *peoplePicker = [[ABPeoplePickerNavigationController alloc] init];
+        peoplePicker.peoplePickerDelegate = self;
+        [self presentViewController:peoplePicker animated:YES completion:nil];
+
+    }
+}
+
+#pragma mark -- CNContactPickerDelegate
+- (void)contactPicker:(CNContactPickerViewController *)picker didSelectContactProperty:(CNContactProperty *)contactProperty {
+    CNPhoneNumber *phoneNumber = (CNPhoneNumber *)contactProperty.value;
+    [self dismissViewControllerAnimated:YES completion:^{
+        /// 联系人
+        NSString *text1 = [NSString stringWithFormat:@"%@%@",contactProperty.contact.familyName,contactProperty.contact.givenName];
+        /// 电话
+        NSString *text2 = phoneNumber.stringValue;
+        //        text2 = [text2 stringByReplacingOccurrencesOfString:@"-" withString:@""];
+        NSLog(@"联系人：%@, 电话：%@",text1,text2);
+    }];
+}
+
+#pragma mark -- ABPeoplePickerNavigationControllerDelegate
+- (void)peoplePickerNavigationController:(ABPeoplePickerNavigationController*)peoplePicker didSelectPerson:(ABRecordRef)person property:(ABPropertyID)property identifier:(ABMultiValueIdentifier)identifier {
+
+    ABMultiValueRef valuesRef = ABRecordCopyValue(person, kABPersonPhoneProperty);
+    CFIndex index = ABMultiValueGetIndexForIdentifier(valuesRef,identifier);
+    CFStringRef value = ABMultiValueCopyValueAtIndex(valuesRef,index);
+    CFStringRef anFullName = ABRecordCopyCompositeName(person);
+
+    [self dismissViewControllerAnimated:YES completion:^{
+        /// 联系人
+        NSString *text1 = [NSString stringWithFormat:@"%@",anFullName];
+        /// 电话
+        NSString *text2 = (__bridge NSString*)value;
+        //        text2 = [text2 stringByReplacingOccurrencesOfString:@"-" withString:@""];
+        NSLog(@"联系人：%@, 电话：%@",text1,text2);
+    }];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (void)getAddressBook {
+    self.myDict = [[NSMutableDictionary alloc]init];
+
+    // 获取指定的字段,并不是要获取所有字段，需要指定具体的字段
+    NSArray *keysToFetch = @[CNContactGivenNameKey, CNContactFamilyNameKey, CNContactPhoneNumbersKey];
+    CNContactFetchRequest *fetchRequest = [[CNContactFetchRequest alloc] initWithKeysToFetch:keysToFetch];
+    CNContactStore *contactStore = [[CNContactStore alloc] init];
+
+    [contactStore enumerateContactsWithFetchRequest:fetchRequest error:nil usingBlock:^(CNContact * _Nonnull contact, BOOL * _Nonnull stop) {
+        NSLog(@"-------------------------------------------------------");
+
+        NSString *givenName = contact.givenName;
+        NSString *familyName = contact.familyName;
+        NSLog(@"givenName=%@, familyName=%@", givenName, familyName);
+
+        NSString *nameStr = [NSString stringWithFormat:@"%@%@",contact.familyName,contact.givenName];
+
+        NSArray *phoneNumbers = contact.phoneNumbers;
+
+        for (CNLabeledValue *labelValue in phoneNumbers) {
+            NSString *label = labelValue.label;
+            NSNumber *phoneNumber = labelValue.value;
+
+            NSLog(@"label=%@, phone=%@", label, phoneNumber.stringValue);
+
+
+        }
+
+//        [_myDict setObject:phoneNumber.stringValue forKey:nameStr];
+
+
+        //    *stop = YES; // 停止循环，相当于break；
+
+    }];
+}
 
 @end
